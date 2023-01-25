dcb6daa5509555e53d9f25a178154e4b1df8a255
Noah Seltzer <noahseltzer@outlook.com>
2023-01-25 14:16:09 -0800

    test:add tests

--- functions/api/[[index]].js
+++ functions/api/[[index]].js
@@ -1,20 +1,60 @@
 /**
+ * Checks is STAGE env var is
+ * in production
+ */
+const isProd = stage => stage === 'prod' || stage === 'production'
+
+/**
+ * Returns correct hostname based on stage
+ * env var
+ */
+const getNewHostname = env => {
+  if (isProd(env.STAGE)) {
+    return 'api.logo.com'
+  }
+  return 'devapi.logo.com'
+}
+
+/**
+ * Generate new request based on
+ * incoming request and new url
+ */
+const generateNewRequest = (url, request) => {
+  const outgoingUrl = url.toString()
+  return new Request(outgoingUrl, request)
+}
+
+/**
+ * Create new url based on env
+ */
+const generateNewUrl = (url, env) => {
+  const newURL = new URL(url)
+
+  newURL.hostname = getNewHostname(env)
+
+  return newURL
+}
+
+/**
  * matches /api/* routes
  * creates new url by changing the hostname
  * routes to api or devapi depending on STAGE env var
  */
 
 const redirect = async ({ request, env }) => {
-    const url = new URL(request.url)
-    if (env.STAGE === 'prod' || env.STAGE === 'production') {
-      url.hostname = 'api.logo.com'
-    } else {
-      url.hostname = 'devapi.logo.com'
-    }
-    const outgoingUrl = url.toString()
-    const outgoingRequest = new Request(outgoingUrl, request)
-    return fetch(outgoingRequest)
-  }
-  
-  export const onRequest = [redirect]
-  
+  const url = generateNewUrl(request.url, env)
+
+  const outgoingRequest = generateNewRequest(url, request)
+
+  return fetch(outgoingRequest)
+}
+
+const onRequest = [redirect]
+
+module.exports = {
+  onRequest,
+  redirect,
+  generateNewRequest,
+  getNewHostname,
+  isProd,
+}
