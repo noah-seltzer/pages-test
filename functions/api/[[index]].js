40d008eb80594446ba1a6e3a693e5bfea956376f
Noah Seltzer <noahseltzer@outlook.com>
2023-01-25 12:58:25 -0800

    fix(envc)

--- functions/api/[[index]].js
+++ functions/api/[[index]].js
@@ -2,12 +2,12 @@ import { isProd } from "../../helpers"
 
 const redirect = async ({ request, next, env }) => {
     const url = new URL(request.url)
-    console.log(env)
+    console.log(env.STAGE)
+    if (env.STAGE === 'prod' || env.STAGE === 'production') {
         url.hostname = 'api.logo.com'
-    // if (isProd()) {
-    // } else {
-    //     url.hostname = 'devapi.logo.com'
-    // }
+    } else {
+        url.hostname = 'devapi.logo.com'
+    }
     const outgoingUrl = url.toString()
     console.log(outgoingUrl)
     const outgoingRequest = new Request(outgoingUrl)
